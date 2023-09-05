9fce30ecfd65c6a0de4d9721681c0007fb4d60d2
wodnj5 <jw991211@gmail.com>
2023-09-05 20:28:39 +0900

    Baekjoon1436

--- src/Baekjoon2839.java
+++ src/Baekjoon2839.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon1018 {
+public class Baekjoon2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        
     }
 }
