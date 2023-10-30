566080201e174ac706df98eec9019db5a56af58b
wodnj5 <jw991211@gmail.com>
2023-10-30 22:14:04 +0900

    feat: 2485

--- src/Baekjoon2485.java
+++ src/Baekjoon2485.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Baekjoon2485 {
 
@@ -27,24 +26,12 @@ public class Baekjoon2485 {
             trees[i] = number;
         }
 
-        int[] space = new int[numberOfTrees-1];
-        for(int i = 0; i < numberOfTrees - 1; i++) {
-            int number = trees[i+1] - trees[i];
-            space[i] = number;
-        }
-        Arrays.sort(space);
-
-        int firstSpace = space[0];
-        int lastSpace = space[numberOfTrees-2];
-        int minimumSpace = gcd(firstSpace, lastSpace);
-
-        int firstTree = trees[0];
-        int lastTree = trees[numberOfTrees - 1];
-        if((lastTree - firstTree) % minimumSpace != 0) {
-            minimumSpace = 1;
+        int minimumSpace = 0;
+        for(int i = 0; i < numberOfTrees-1; i++) {
+            minimumSpace = gcd(minimumSpace, trees[i+1] - trees[i]);
         }
 
-        int result = ((lastTree - firstTree) / minimumSpace + 1) - numberOfTrees;
+        int result = ((trees[numberOfTrees-1] - trees[0]) / minimumSpace) + 1 - numberOfTrees;
         bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
